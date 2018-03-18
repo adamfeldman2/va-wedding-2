ee66e12e42116c7b2e0156a98e6c461f472d3f61
Adam <feldman238@gmail.com>
2018-03-18 12:52:10 -0400

    Build and style hamburger menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,33 +1,68 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <div className="wrapper-component-header wrapper">
-      <ul className="wrapper">
-        <li>
-          <NavLink to="/details" activeClassName="active">
-            Details
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/lodging" activeClassName="active">
-            Lodging
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/rsvp" activeClassName="active">
-            RSVP
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" activeClassName="active">
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </div>
-  );
-};
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hamburgerActive: false
+    };
+
+    this.onHamburgerClick = this.onHamburgerClick.bind(this);
+  }
+
+  onHamburgerClick() {
+    this.setState((prevState) => {
+      return {
+        hamburgerActive: !prevState.hamburgerActive
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="wrapper-component-header wrapper">
+        {this.state.hamburgerActive ? (
+          <button className="hamburger active" onClick={this.onHamburgerClick}>
+            &times;
+          </button>
+        ) : (
+          <button className="hamburger" onClick={this.onHamburgerClick}>
+            &#9776;
+          </button>
+        )}
+
+        <ul className={`wrapper ${this.state.hamburgerActive ? 'active' : ''}`}>
+          <li>
+            <NavLink to="/" className="nav-link-home" onClick={this.onHamburgerClick}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/details" activeClassName="active" onClick={this.onHamburgerClick}>
+              Details
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/lodging" activeClassName="active" onClick={this.onHamburgerClick}>
+              Lodging
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/rsvp" activeClassName="active" onClick={this.onHamburgerClick}>
+              RSVP
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" activeClassName="active" onClick={this.onHamburgerClick}>
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Header;
