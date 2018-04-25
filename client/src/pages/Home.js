79e598c2accc41a799ade0c4911ae15d62240263
Adam <feldman238@gmail.com>
2018-04-24 22:13:30 -0400

    Update background photo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="wrapper-page-home">
       <Link to="/" className="wrapper-content-home">
-        <h1>VIVIANA &amp; ADAM</h1>
+        <h1>Viviana &amp; Adam</h1>
         <h5>August 4, 2018</h5>
         <Countdown endDate={'August 4, 2018 18:00'} />
       </Link>
